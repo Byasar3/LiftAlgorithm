64752374ab1f2ef8849eee1f57789fae402be8cd
Berna <bernayasar@hotmail.co.uk>
2023-05-09 23:18:15 +0100

    made person class with properties and getters and setters

---
+++ Person.cs
@@ -0,0 +1,7 @@
+public class Person 
+{
+    public int Id { get; set; }
+    public int CurrentFloor { get; set; }
+    public int TargetFloor { get; set; }
+    public DateTime CallTime { get; set;}
+}
