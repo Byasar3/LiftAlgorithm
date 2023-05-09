bd125d648446f1bad27e300d143c36406392131b
Berna Yasar <Berna@Bernas-MacBook-Air.local>
2023-05-09 22:56:14 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.VisualBasic.FileIO;
+
+string filePath = "/Users/Berna/Documents/LiftAlgorithm/DataInput.csv";
+
+using (TextFieldParser parser = new TextFieldParser(filePath)) 
+{
+    // break up csv file with ","
+    parser.Delimiters = new string[] { "," };
+
+    Console.WriteLine(parser);
+
+    // read the csv file row by row:
+    while (!parser.EndOfData)
+    {
+        // added in '?' as it was failing to run incase the csv file was null
+        string[]? fields = parser.ReadFields();
+
+        Console.WriteLine(fields[0]);
+        Console.WriteLine(fields[1]);
+        Console.WriteLine(fields[2]);
+        Console.WriteLine(fields[3]);
+    }
+}
+
+
+
