0ad5b348be17b62bd3e89fbd2ae9b7a9a6e2dd36
Berna <bernayasar@hotmail.co.uk>
2023-05-15 19:20:39 +0100

    deleted CsvParser class

--- Program.cs
+++ Program.cs
@@ -1,37 +1,7 @@
-﻿/*
-using Microsoft.VisualBasic.FileIO;
-
-string filePath = "/Users/Berna/Documents/LiftAlgorithm/DataInput.csv";
-
-using (TextFieldParser parser = new TextFieldParser(filePath)) 
-{
-    // break up csv file with ","
-    parser.Delimiters = new string[] { "," };
-
-    Console.WriteLine(parser);
-
-    // read the csv file row by row:
-    while (!parser.EndOfData)
-    {
-        // added in '?' as it was failing to run incase the csv file was null
-        string[]? fields = parser.ReadFields();
-
-        Console.WriteLine(fields[0]);
-        Console.WriteLine(fields[1]);
-        Console.WriteLine(fields[2]);
-        Console.WriteLine(fields[3]);
-
-    }
-
-}
-*/
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
-    {
-         // List<Person> personsFromData = CsvParser.ParsePerson("/Users/Berna/Documents/LiftAlgorithm/DataInput.csv");
-        
+    {        
         //making a list of people to store the csv data
         List<Person> people = new List<Person>();
 
